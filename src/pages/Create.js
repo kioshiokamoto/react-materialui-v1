6edf93cf1e7361c1c28c14e9818c267a598c811d
Kioshi Okamoto <kjor29@gmail.com>
2021-03-16 19:03:43 -0500

    :fire: IMPROVE: Agregar iconos y botones de material-ui

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React from 'react';
+import { Typography, Button, Container } from '@material-ui/core';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 export default function Create() {
-  return (
-    <div>
-      Create page
-    </div>
-  )
+	return (
+		<Container>
+			<Typography variant="h6" color="textSecondary" component="h2" gutterBottom>
+				Create a new note
+			</Typography>
+			<Button 
+        onClick={() => console.log(`Work`)} 
+        type="submit" 
+        color="secondary" 
+        variant="contained"
+        endIcon={<KeyboardArrowRightIcon />}
+      >
+				Submit
+			</Button>
+
+		</Container>
+	);
 }
