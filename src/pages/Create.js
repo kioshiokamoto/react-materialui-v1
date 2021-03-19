c4d8a436978dbcf46ab70444ed9e176996b17f34
Kioshi Okamoto <kjor29@gmail.com>
2021-03-19 13:41:21 -0500

    :fire: IMPROVE: Creacion de formulario y validacion sencilla

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,22 +1,72 @@
 import React from 'react';
-import { Typography, Button, Container } from '@material-ui/core';
+import { Typography, Button, Container, makeStyles, TextField } from '@material-ui/core';
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import { useState } from 'react';
+
+const useStyles = makeStyles({
+	field: {
+		marginTop: 20,
+		marginBottom: 20,
+		display: 'block',
+	},
+});
+
 export default function Create() {
+	const classes = useStyles();
+	const [title, setTitle] = useState('');
+	const [details, setDetails] = useState('');
+
+	const [titleError, setTitleError] = useState(false);
+	const [detailsError, setDetailsError] = useState(false);
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setTitleError(false);
+		setDetailsError(false);
+		
+		if (title === '') {
+			setTitleError(true);
+		}
+		if (details === '') {
+			setDetailsError(true);
+		}
+
+		if (title && details) {
+			console.log(title, details);
+		}
+	};
+
 	return (
 		<Container>
 			<Typography variant="h6" color="textSecondary" component="h2" gutterBottom>
 				Create a new note
 			</Typography>
-			<Button 
-        onClick={() => console.log(`Work`)} 
-        type="submit" 
-        color="secondary" 
-        variant="contained"
-        endIcon={<KeyboardArrowRightIcon />}
-      >
-				Submit
-			</Button>
-
+			<form noValidate autoComplete="off" onSubmit={handleSubmit}>
+				<TextField
+					onChange={(e) => setTitle(e.target.value)}
+					className={classes.field}
+					label="Note title"
+					variant="outlined"
+					color="secondary"
+					fullWidth
+					required
+					error={titleError}
+				/>
+				<TextField
+					onChange={(e) => setDetails(e.target.value)}
+					className={classes.field}
+					label="Details"
+					variant="outlined"
+					color="secondary"
+					multiline
+					rows={4}
+					fullWidth
+					required
+					error={detailsError}
+				/>
+				<Button type="submit" color="secondary" variant="contained" endIcon={<KeyboardArrowRightIcon />}>
+					Submit
+				</Button>
+			</form>
 		</Container>
 	);
 }
