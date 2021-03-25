48b60f9da03f21d853bc88b1132d4661cc64d6c5
Kioshi Okamoto <kjor29@gmail.com>
2021-03-24 19:03:48 -0500

    :dart: IMPROVE: delete notes - json server

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Container, Grid, Paper } from '@material-ui/core';
+import NoteCard from '../components/NoteCard'
 export default function Notes() {
 	const [notes, setNotes] = useState([]);
 	useEffect(() => {
@@ -11,13 +12,22 @@ export default function Notes() {
 		};
 		getData();
 	}, []);
+
+	const handleDelete = async (id)=>{
+		await fetch('http://localhost:8000/notes/'+id, {
+			method: 'DELETE'
+		});
+		const newNotes = notes.filter((note)=> note.id !== id);
+		setNotes(newNotes);
+	}
+
+
 	return (
 		<Container>
-			<Grid container>
+			<Grid container spacing={3}>
 				{notes.map((note) => (
-					<Grid item xs={12} sm={6} md={4} key={note.id}>
-						<Paper>{note.title}</Paper>
-
+					<Grid item xs={12} md={6} lg={4} key={note.id}>
+						<NoteCard note={note} handleDelete={handleDelete} />
 					</Grid>
 				))}
 			</Grid>
