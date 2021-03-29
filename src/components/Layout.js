cffb4f8dd452f36cbb55ac515515143e2be503ef
Kioshi Okamoto <kjor29@gmail.com>
2021-03-29 16:33:44 -0500

    :dart: IMPROVE: Modificar makeStyles utilizando theme y pruebas en estilo con condiciones

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import { makeStyles, Drawer, Typography } from '@material-ui/core';
+import { makeStyles, Drawer, Typography, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import { AddCircleOutlineOutlined, SubjectOutlined } from '@material-ui/icons';
+import { useHistory, useLocation } from 'react-router';
 
 const drawerWidth = 240
 
 
-const useStyles = makeStyles({
-    root:{
-        display:'flex'
-    },
-	page: {
-		background: '#f9f9f9',
-		width: '100%',
-	},
-    drawer:{
-        width:drawerWidth
-    },
-    drawPaper:{
-        width:drawerWidth
+const useStyles = makeStyles((theme)=>{
+    return {
+        root:{
+            display:'flex'
+        },
+        page: {
+            background: '#f9f9f9',
+            width: '100%',
+            padding:theme.spacing(3)
+        },
+        drawer:{
+            width:drawerWidth
+        },
+        drawPaper:{
+            width:drawerWidth
+        },
+        active:{
+            background:'#f4f4f4'
+        },
+        title:{
+            padding: theme.spacing(2)
+        }
     }
 });
 const Layout = ({ children }) => {
 	const classes = useStyles();
+    const history = useHistory();
+    const location = useLocation();
+    const menuItems= [
+        {
+            text:'My notes',
+            icon: <SubjectOutlined color="secondary" />,
+            path:'/'
+        },
+        {
+            text:'Create Notes',
+            icon: <AddCircleOutlineOutlined color="secondary" />,
+            path:'/create'
+        },
+    ]
+
 	return (
 		<div className={classes.root}>
             {/* appbar */}
@@ -33,11 +59,27 @@ const Layout = ({ children }) => {
                 classes={{paper: classes.drawPaper}}
             >
                 <div>
-                    <Typography variant="h5">
+                    <Typography variant="h5" className={classes.title}>
                         Ninja Notes
                     </Typography>
                 </div>
+
+                <List>
+                    {menuItems.map(item => (
+                        <ListItem 
+                            button
+                            key={item.text}
+                            onClick={()=>history.push(item.path)}
+                            className={location.pathname== item.path ? classes.active : null}
+                        >
+                            <ListItemIcon>{item.icon}</ListItemIcon>
+                            <ListItemText primary={item.text}/>
+                        </ListItem>
+                    ))}
+                </List>
+
             </Drawer>
+
 
 			<div className={classes.page}>
                 {children}
--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -1,27 +1,43 @@
-import React from 'react'
-import { Card, CardContent, CardHeader, IconButton, Typography} from '@material-ui/core'
-import {DeleteOutlined }from '@material-ui/icons'
-const NoteCard = ({note,handleDelete}) => {
-    return (
-        <div>
-            <Card elevation={1} >
-                <CardHeader 
-                    action={
-                        <IconButton onClick={()=>{handleDelete(note.id)}}>
-                          <DeleteOutlined />
-                        </IconButton>
-                      }
-                    title={note.title}
-                    subheader={note.category}
-                />
-                <CardContent>
-                    <Typography variant="body2" color="textSecondary" >
-                        {note.details}
-                    </Typography>
-                </CardContent>
-            </Card>
-        </div>
-    )
-}
+import React from 'react';
+import { Card, CardContent, CardHeader, IconButton, makeStyles, Typography } from '@material-ui/core';
+import { DeleteOutlined } from '@material-ui/icons';
 
-export default NoteCard
+const useStyles = makeStyles({
+	test: {
+		border: (note) => {
+			if (note.category == 'work') {
+				return '1px solid red';
+			}
+		},
+	},
+});
+
+const NoteCard = ({ note, handleDelete }) => {
+	const classes = useStyles(note);
+	return (
+		<div>
+			<Card elevation={1} className={classes.test}>
+				<CardHeader
+					action={
+						<IconButton
+							onClick={() => {
+								handleDelete(note.id);
+							}}
+						>
+							<DeleteOutlined />
+						</IconButton>
+					}
+					title={note.title}
+					subheader={note.category}
+				/>
+				<CardContent>
+					<Typography variant="body2" color="textSecondary">
+						{note.details}
+					</Typography>
+				</CardContent>
+			</Card>
+		</div>
+	);
+};
+
+export default NoteCard;
