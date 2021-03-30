276370c0169fd1acd1ca001add6c50f82afbf955
Kioshi Okamoto <kjor29@gmail.com>
2021-03-30 18:35:20 -0500

    :dart: IMPROVE: implementacion de appbar y agregar avatares a notas

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,89 +1,114 @@
 import React from 'react';
-import { makeStyles, Drawer, Typography, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import {
+	makeStyles,
+	Drawer,
+	Typography,
+	List,
+	ListItem,
+	ListItemIcon,
+	ListItemText,
+	AppBar,
+	Toolbar,
+	Avatar
+} from '@material-ui/core';
 import { AddCircleOutlineOutlined, SubjectOutlined } from '@material-ui/icons';
 import { useHistory, useLocation } from 'react-router';
+import { format } from 'date-fns';
 
-const drawerWidth = 240
+const drawerWidth = 240;
 
-
-const useStyles = makeStyles((theme)=>{
-    return {
-        root:{
-            display:'flex'
+const useStyles = makeStyles((theme) => {
+	return {
+		root: {
+			display: 'flex',
+		},
+		page: {
+			background: '#f9f9f9',
+			width: '100%',
+			padding: theme.spacing(3),
+		},
+		drawer: {
+			width: drawerWidth,
+		},
+		drawPaper: {
+			width: drawerWidth,
+		},
+		active: {
+			background: '#f4f4f4',
+		},
+		title: {
+			padding: theme.spacing(2),
+		},
+		appbar: {
+			width: `calc(100% - ${drawerWidth}px)`,
+		},
+		toolbar: theme.mixins.toolbar,
+        date:{
+            flexGrow:1
         },
-        page: {
-            background: '#f9f9f9',
-            width: '100%',
-            padding:theme.spacing(3)
-        },
-        drawer:{
-            width:drawerWidth
-        },
-        drawPaper:{
-            width:drawerWidth
-        },
-        active:{
-            background:'#f4f4f4'
-        },
-        title:{
-            padding: theme.spacing(2)
-        }
-    }
+		avatar:{
+			marginLeft:theme.spacing(2)
+		}
+	};
 });
 const Layout = ({ children }) => {
 	const classes = useStyles();
-    const history = useHistory();
-    const location = useLocation();
-    const menuItems= [
-        {
-            text:'My notes',
-            icon: <SubjectOutlined color="secondary" />,
-            path:'/'
-        },
-        {
-            text:'Create Notes',
-            icon: <AddCircleOutlineOutlined color="secondary" />,
-            path:'/create'
-        },
-    ]
+	const history = useHistory();
+	const location = useLocation();
+	const menuItems = [
+		{
+			text: 'My notes',
+			icon: <SubjectOutlined color="secondary" />,
+			path: '/',
+		},
+		{
+			text: 'Create Notes',
+			icon: <AddCircleOutlineOutlined color="secondary" />,
+			path: '/create',
+		},
+	];
 
 	return (
 		<div className={classes.root}>
-            {/* appbar */}
-
-            {/* side drawer */}
-            <Drawer
-                className={classes.drawer}
-                variant="permanent"
-                anchor="left"
-                classes={{paper: classes.drawPaper}}
-            >
-                <div>
-                    <Typography variant="h5" className={classes.title}>
-                        Ninja Notes
+			{/* appbar */}
+			<AppBar className={classes.appbar} elevation={0}>
+				<Toolbar>
+					<Typography className={classes.date}>
+                        Today is the {format(new Date(),'do MMMM Y')}
                     </Typography>
-                </div>
+                    <Typography>
+                        Kioshi
+                    </Typography>
+					<Avatar src="/mario-av.png" className={classes.avatar}/>
+				</Toolbar>
+			</AppBar>
+			{/* side drawer */}
+			<Drawer className={classes.drawer} variant="permanent" anchor="left" classes={{ paper: classes.drawPaper }}>
+				<div>
+					<Typography variant="h5" className={classes.title}>
+						Ninja Notes
+					</Typography>
+				</div>
 
-                <List>
-                    {menuItems.map(item => (
-                        <ListItem 
-                            button
-                            key={item.text}
-                            onClick={()=>history.push(item.path)}
-                            className={location.pathname== item.path ? classes.active : null}
-                        >
-                            <ListItemIcon>{item.icon}</ListItemIcon>
-                            <ListItemText primary={item.text}/>
-                        </ListItem>
-                    ))}
-                </List>
-
-            </Drawer>
-
+				<List>
+					{menuItems.map((item) => (
+						<ListItem
+							button
+							key={item.text}
+							onClick={() => history.push(item.path)}
+							className={location.pathname == item.path ? classes.active : null}
+						>
+							<ListItemIcon>{item.icon}</ListItemIcon>
+							<ListItemText primary={item.text} />
+						</ListItem>
+					))}
+				</List>
+			</Drawer>
 
 			<div className={classes.page}>
-                {children}
-            </div>
+				<div className={classes.toolbar}></div>
+				{children}
+			</div>
 		</div>
 	);
 };
--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -1,23 +1,36 @@
 import React from 'react';
-import { Card, CardContent, CardHeader, IconButton, makeStyles, Typography } from '@material-ui/core';
+import { Avatar, Card, CardContent, CardHeader, IconButton, makeStyles, Typography } from '@material-ui/core';
 import { DeleteOutlined } from '@material-ui/icons';
+import { blue, green, pink, yellow } from '@material-ui/core/colors';
 
 const useStyles = makeStyles({
-	test: {
-		border: (note) => {
-			if (note.category == 'work') {
-				return '1px solid red';
+	avatar:{
+		backgroundColor: (note) =>{
+			if(note.category === 'work'){
+				return yellow[700]
 			}
-		},
-	},
+			if(note.category === 'money'){
+				return green[500]
+			}
+			if(note.category === 'todos'){
+				return pink[500]
+			}
+			return blue[500]
+		}
+	}
 });
 
 const NoteCard = ({ note, handleDelete }) => {
 	const classes = useStyles(note);
 	return (
 		<div>
-			<Card elevation={1} className={classes.test}>
+			<Card elevation={1} >
 				<CardHeader
+					avatar={
+						<Avatar className={classes.avatar} >
+							{note.category[0].toUpperCase()}
+						</Avatar>
+					}
 					action={
 						<IconButton
 							onClick={() => {
